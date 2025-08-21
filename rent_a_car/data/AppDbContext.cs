70fdd34e14772f97abc7dcd4c49facb0a4978758
hara2205 <hsemic5@gmail.com>
2025-08-21 15:00:17 +0200

    Opis izmjena, npr. updated DB connection string

--- rent_a_car/data/AppDbContext.cs
+++ rent_a_car/data/AppDbContext.cs
@@ -13,5 +13,33 @@ namespace rent_a_car.data
         public DbSet<Vehicle> Vehicles { get; set; } = null!;
         public DbSet<Users> Users { get; set; } = null!;
         public DbSet<Reservation> Reservations { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Primarni ključevi
+            modelBuilder.Entity<Users>()
+                .HasKey(u => u.UserID);
+
+            modelBuilder.Entity<Vehicle>()
+                .HasKey(v => v.VehicleID);
+
+            modelBuilder.Entity<Reservation>()
+                .HasKey(r => r.ReservationID);
+
+            // Relacije
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.User)
+                .WithMany()
+                .HasForeignKey(r => r.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Reservation>()
+                .HasOne(r => r.Vehicle)
+                .WithMany()
+                .HasForeignKey(r => r.VehicleID)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
